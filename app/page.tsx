1e84e84c6afa489d780e558380e6188df217c8b4
daross01 <darosscontact@gmail.com>
2026-06-08 15:34:59 +0200

    Initial commit - Dev Academy setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Dev Academy 🚀
+      </h1>
+
+      <p className="mt-4 text-gray-500">
+        Mi primera app profesional con Next.js
+      </p>
+    </main>
+  );
+}
